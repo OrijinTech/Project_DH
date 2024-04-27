16ce8ce05cb6758cb64506feae9070c95ab16230
YongxiangJ <yongxiangj1999@gmail.com>
2024-04-27 16:31:19 +0200

    initial commit

---
+++ Project_Me/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project_Me
+//
+//  Created by Yongxiang Jin on 4/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project_Me/Project_MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_MeApp.swift
+//  Project_Me
+//
+//  Created by Yongxiang Jin on 4/27/24.
+//
+
+import SwiftUI
+
+@main
+struct Project_MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
